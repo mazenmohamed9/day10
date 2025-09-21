c2ffd73a91fccaccf4b6dda33f5fc1eebe22b8b7
Mazen Mohamed <mazenlearn1@gmail.com>
2025-09-21 10:14:53 +0300

    maz

---
+++ Assignment1.dart
@@ -0,0 +1,54 @@
+/*1. Difference between Class and Object
+
+Class: A blueprint/template that defines the properties (variables) and behaviors (methods) of something.
+
+Object: A real instance created from a class.
+
+Real-life example:
+
+A Class could be Car (it defines brand, color, speed, and methods like drive or stop).
+
+An Object is your actual Toyota Corolla (red, 180km/h) created from that class.
+
+
+2. What is a Constructor? Types in Dart
+
+A Constructor is a special method used to create and initialize objects.
+
+Types of Constructors in Dart:
+
+Default Constructor.
+
+Parameterized Constructor.
+
+Named Constructor.
+
+Factory Constructor.
+
+
+3. Access Modifiers in Dart
+
+Dart does not have public, private, protected like Java.
+
+Instead, it uses library-level privacy:
+
+Any identifier starting with _ is private to its library.
+
+Otherwise, it’s public.
+
+
+4. Encapsulation in Dart
+
+Encapsulation wrapping data and methods together while hiding implementation details.
+
+
+5. Why use a Setter instead of making a variable public?
+
+ If you make a variable public:
+Anyone can directly modify it without restriction.
+No validation or control.
+
+With a setter:
+You control how the value is set.
+You keep the object’s data safe and consistent
+*/
